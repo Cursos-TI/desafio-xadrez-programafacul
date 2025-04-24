2ee438d6a120f3531ba9feb99025df6d7c54a393
programafacul <aziatico09zorra@gmail.com>
2025-04-24 10:55:01 -0300

    Update xadrez.c
    .

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
-
 int main() {
-   
+    // mover a torre 5 casas para a direita 
+    printf("\n Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n"); // imprime a direção do movimento 
+    }
+    printf("Torre se moveu 5 casas para a direita.\n");
 
+    // mover o Bispo 5 casas para a direita para cima 
+    printf("\n Bispo:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Cima, Direita\n"); // imprime a direção do movimento 
+    }
+    printf("Bispo se moveu 5 casas a direita para cima .\n");
 
-
-
-
-    return 0;
+        // mover o Rainha 8 casas para a esquerda 
+        printf("\n Rainha:\n");
+        for (int i = 0; i < 8; i++) {
+            printf("Esquerda\n"); // imprime a direção do movimento 
+        }
+        printf("Rainha se moveu 8 casas a esquerda .\n");
+    
+    return 0;    
 }
